e06cb8fff0c7766e4fbb76bf1aa99de4145f7464
arhefr <efremenkoarhip614090@gmail.com>
2025-10-23 18:43:25 +0300

    add logger package

---
+++ logger/logger.go
@@ -0,0 +1,79 @@
+package logger
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	loggerRequestIDKey string = "x-request-id"
+	loggerTraceIDKey   string = "x-trace-id"
+)
+
+type Logger interface {
+	Warn(ctx context.Context, msg string, fields ...zap.Field)
+	Debug(ctx context.Context, msg string, fields ...zap.Field)
+	Error(ctx context.Context, msg string, fields ...zap.Field)
+	Fatal(ctx context.Context, msg string, fields ...zap.Field)
+	Info(ctx context.Context, msg string, fields ...zap.Field)
+}
+
+func NewLogger(logLevel zapcore.Level) (Logger, error) {
+	loggerCfg := zap.NewProductionConfig()
+
+	loggerCfg.Level = zap.NewAtomicLevelAt(logLevel)
+
+	logger, err := loggerCfg.Build()
+	if err != nil {
+		return nil, err
+	}
+	defer logger.Sync()
+
+	return &L{z: logger}, nil
+}
+
+type L struct {
+	z *zap.Logger
+}
+
+func (l L) Debug(ctx context.Context, msg string, fields ...zap.Field) {
+	requestID, traceID := ctx.Value(loggerRequestIDKey), ctx.Value(loggerTraceIDKey)
+
+	fields = append(fields, zap.Any(loggerRequestIDKey, requestID), zap.Any(loggerTraceIDKey, traceID))
+
+	l.z.Debug(msg, fields...)
+}
+
+func (l L) Info(ctx context.Context, msg string, fields ...zap.Field) {
+	requestID, traceID := ctx.Value(loggerRequestIDKey), ctx.Value(loggerTraceIDKey)
+
+	fields = append(fields, zap.Any(loggerRequestIDKey, requestID), zap.Any(loggerTraceIDKey, traceID))
+
+	l.z.Info(msg, fields...)
+}
+
+func (l L) Warn(ctx context.Context, msg string, fields ...zap.Field) {
+	requestID, traceID := ctx.Value(loggerRequestIDKey), ctx.Value(loggerTraceIDKey)
+
+	fields = append(fields, zap.Any(loggerRequestIDKey, requestID), zap.Any(loggerTraceIDKey, traceID))
+
+	l.z.Warn(msg, fields...)
+}
+
+func (l L) Error(ctx context.Context, msg string, fields ...zap.Field) {
+	requestID, traceID := ctx.Value(loggerRequestIDKey), ctx.Value(loggerTraceIDKey)
+
+	fields = append(fields, zap.Any(loggerRequestIDKey, requestID), zap.Any(loggerTraceIDKey, traceID))
+
+	l.z.Error(msg, fields...)
+}
+
+func (l L) Fatal(ctx context.Context, msg string, fields ...zap.Field) {
+	requestID, traceID := ctx.Value(loggerRequestIDKey), ctx.Value(loggerTraceIDKey)
+
+	fields = append(fields, zap.Any(loggerRequestIDKey, requestID), zap.Any(loggerTraceIDKey, traceID))
+
+	l.z.Fatal(msg, fields...)
+}
