ed4ae964f5c06ad7b5444a94fd5c4302b0b46213
arhefr <efremenkoarhip614090@gmail.com>
2025-10-26 12:33:08 +0300

    add env tags

--- postgrespool/config.go
+++ postgrespool/config.go
@@ -1,11 +1,11 @@
 package postgrespool
 
 type Config struct {
-	User         string
-	Password     string
-	Host         string
-	Port         string
-	Database     string
-	MaxAttemps   int64
-	DelayAttemps int64
+	User         string `env:"POSTGRES_USER" env-default:"postgres"`
+	Password     string `env:"POSTGRES_PASSWORD" env-default:"postgres"`
+	Host         string `env:"POSTGRES_HOST" env-default:"localhost"`
+	Port         string `env:"POSTGRES_PORT" env-default:"5432"`
+	Database     string `env:"POSTGRES_DATABASE" env-default:"postgres"`
+	MaxAttemps   int64  `env:"POSTGRES_ATTEMPS" env-default:"5"`
+	DelayAttemps int64  `env:"POSTGRES_ATTEMPS_DELAY" env-default:"3"`
 }
